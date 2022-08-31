1635a68de5c8dc33b480b0faba8033606ce4514a
Riboz <74473384+Riboz@users.noreply.github.com>
2022-08-31 03:04:20 +0300

    addmobs are added only need to login your addmob id for android

--- Assets/codes/buttoncode.cs
+++ Assets/codes/buttoncode.cs
@@ -2,12 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using GoogleMobileAds.Api;
 public class buttoncode : MonoBehaviour
 {
+    private InterstitialAd interstitial;
+    //resources da android kısmına kendi id ni yazman lazım
 
+private void RequestInterstitial()
+{
+    #if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+    #elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+    #else
+        string adUnitId = "unexpected_platform";
+    #endif
+    // Initialize an InterstitialAd.
+    this.interstitial = new InterstitialAd(adUnitId);
+    
+      AdRequest request = new AdRequest.Builder().Build();
+
+    // Load the interstitial with the request.
+    this.interstitial.LoadAd(request);
+}
+
+
+  
+
+private void GameOver()
+{
+  if (this.interstitial.IsLoaded()) {
+    this.interstitial.Show();
+  }
+}
+
+
+    public void Start()
+    {
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(initStatus => { });
+        }
+
+        
+public IEnumerator a()
+{
+    GameOver();
+    yield return new WaitForSeconds(4f);
+    interstitial.Destroy();
+
+    SceneManager.LoadScene("level");
+    yield break;
+}
 public void oyungir()
 {
-    SceneManager.LoadScene("level");
+
+    StartCoroutine(a());
     badfatboy.gamestart=false;
     Time.timeScale=1;
 }
